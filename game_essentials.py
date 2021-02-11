1f7cbc7485fa5d9f692c3199a3b2956c916581aa
AkshajC <akshajchalla@yahoo.com>
2021-02-11 10:46:45 -0800

    Fixed logic issue with janitor.

--- game_essentials.py
+++ game_essentials.py
@@ -2,6 +2,7 @@ class Game:
     def __init__(self, players):
         self.players = players
         self.is_day = True
+        self.dead_players = []
         self.num_living = len(players)
 
     def turn_night(self):
@@ -12,13 +13,20 @@ class Game:
             if killer.role.__class__.__name__ == "Mafioso" or killer.role.__class__.__name__ == "Godfather":
                 message = f"{target.name} was killed by the mafia."
                 recipient = "everyone"
+                self.dead_players.append(target)
                 del self.players[self.players.index(target)]
-            elif killer.role.__class__.__name__ == "Janitor":
-                message = f"{target.name} was cleaned"
-                recipient = "everyone"
-                del self.players[self.players.index(target)]
+            for elem in self.players:
+                if elem[2].role.__class__.__name__ == "Janitor" and elem[2].role.is_clean is True and elem[2].role.target is target:
+                    message = f"{target.name} was cleaned."
+
+
+
 
         else:
             message = "Your target's defense is too high."
             recipient = killer
         return [message, recipient]
+
+class Player:
+    def __init__(self, name, role, number):
+        self.name = name
--- role_classes.py
+++ role_classes.py
@@ -1,1 +1,5 @@
-
+class Vigilante:
+    def __init__(self):
+        self.faction = "Town"
+        self.defense = 0
+        self.attack = 1
